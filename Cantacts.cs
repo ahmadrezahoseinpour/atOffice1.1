26b3277aac4c6a76a51f3da2fb376214f341ea7f
ahmadreza_hoseinpour <ahmadrezahp13802@gmai.com>
2023-01-04 11:09:59 +0330

    last version of 1.1

---
+++ Cantacts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Contacts
+{
+    class Cantacts : IContacts
+    {
+        private string connections = "Data Source = .; Initial catalog = Contact_DB; Inegrated Security = true";
+        public bool Delete(int contactid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string name, string family, string email, string address, int age)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectRow(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int contactId, string name, string family, string email, string address, int age)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IContacts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Contacts
+{
+    interface IContacts
+    {
+        //Some Methods ...
+        DataTable SelectAll();
+        DataTable SelectRow(int contactId);
+
+        //some anothers insert, update, delete
+
+        bool Insert(string name, string family, string email, string address, int age);
+        bool Update(int contactId, string name, string family, string email, string address, int age);
+        bool Delete(int contactid);
+
+
+
+    }
+}
